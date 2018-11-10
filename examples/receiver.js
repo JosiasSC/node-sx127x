da9bb8ff9580bd6b4be55ca244eb6a3236d6dd6e
Josias Cavalcanti <josias.cavalcanti@gmail.com>
2018-11-10 11:51:39 -0300

    Retirado o comportamento asincrono indevido.

--- examples/receiver.js
+++ examples/receiver.js
@@ -7,28 +7,26 @@ var sx127x = new SX127x({
 var count = 0;
 
 // open the device
-sx127x.open(function(err) {
-  console.log('open', err ? err : 'success');
+sx127x.open();
+console.log('open', 'success');
 
-  if (err) {
-    throw err;
-  }
-
-  // add a event listener for data events
-  sx127x.on('data', function(data, rssi) {
-    console.log('data:', '\'' + data.toString() + '\'', rssi);
-  });
-
-  // enable receive mode
-  sx127x.receive(function(err) {
-    console.log('receive', err ? err : 'success');
-  });
+// add a event listener for data events
+sx127x.on('data', function(data, rssi) {
+  console.log('data:', '\'' + data.toString() + '\'', rssi);
 });
+
+// enable receive mode
+sx127x.receive();
+console.log('receive', 'success');
 
 process.on('SIGINT', function() {
   // close the device
-  sx127x.close(function(err) {
-    console.log('close', err ? err : 'success');
-    process.exit();
-  });
+  try {
+    sx127x.close();
+    console.log('close', 'success');
+  } catch (err) {
+    console.log('close', err);
+  }
+  process.exit();
 });
+
--- examples/sender.js
+++ examples/sender.js
@@ -7,26 +7,26 @@ var sx127x = new SX127x({
 var count = 0;
 
 // open the device
-sx127x.open(function(err) {
-  console.log('open', err ? err : 'success');
+sx127x.open();
 
-  if (err) {
-    throw err;
+// send a message every second
+setInterval(function() {
+  console.log('write: hello ' + count);
+  try {
+    sx127x.write(new Buffer('hello ' + count++));
+    console.log('\t', 'success');
+  } catch (err) {
+    console.log('\t', err);
   }
-
-  // send a message every second
-  setInterval(function() {
-    console.log('write: hello ' + count);
-    sx127x.write(new Buffer('hello ' + count++), function(err) {
-      console.log('\t', err ? err : 'success');
-    });
-  }, 1000);
-});
+}, 1000);
 
 process.on('SIGINT', function() {
   // close the device
-  sx127x.close(function(err) {
-    console.log('close', err ? err : 'success');
-    process.exit();
-  });
+  try {
+    sx127x.close();
+    console.log('close', 'success');
+  } catch (err) {
+    console.log('close', err);
+  }
+  process.exit();
 });
