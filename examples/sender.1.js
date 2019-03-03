c2d19fe3acbeefb46055a8636f25afe6a0887201
Josias Cavalcanti <josias.cavalcanti@gmail.com>
2019-03-03 18:55:02 -0300

    Adicionando intervalo de tempo no envio do novo sender

--- examples/sender.1.js
+++ examples/sender.1.js
@@ -127,6 +127,7 @@ for (var i = 0; i < 255; i++) {
   this._writeRegister(REG_PAYLOAD_LENGTH, 1);
   this._writeRegister(REG_FIFO, i);
   this._writeRegister(REG_OP_MODE, MODE_LONG_RANGE_MODE | MODE_TX);
+  sleep.msleep(1000);
 }
 
 /*
